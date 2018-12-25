4d79857a3f42a8e87badd65d13efaa89aabd77fb
ydi <ydi@ciklum.net>
2018-12-25 19:53:16 +0200

    added remote wd connection

--- tests/base.py
+++ tests/base.py
@@ -1,14 +1,32 @@
 import pytest
+from selene import config, browser
 from selene.browser import set_driver, driver
+from selene.browsers import BrowserName
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
+
+desired_cap = {'browserName': 'chrome',
+               'version': '69.0',
+               'enableVNC': True,
+               'javascriptEnabled': True}
 
 
 class BaseTest(object):
 
     @pytest.fixture(scope="session", autouse=True)
     def setup(self):
-        set_driver(webdriver.Chrome(ChromeDriverManager().install()))
+        # chrome_options = webdriver.ChromeOptions()
+        # chrome_options.add_argument("--disable-notifications")
+        # chrome_options.add_argument("--enable-automation")
+        # chrome_options.add_argument("--start-maximized")
+        #
+        # driver = webdriver.Remote(
+        #     command_executor='http://selenoid:selenoid@172.28.28.217:4445/wd/hub',
+        #     desired_capabilities=desired_cap)
 
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+
+        browser.set_driver(driver)
         yield
-        driver().quit()
+        browser.quit()
