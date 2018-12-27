8230c1ba1c2b59bf04a54d5435141c6685dd7cbf
ydi <ydi@ciklum.net>
2018-12-27 17:36:01 +0200

    minor fixes

--- tests/base.py
+++ tests/base.py
@@ -1,3 +1,5 @@
+import unittest
+
 import pytest
 from selene import browser, config
 from selenium import webdriver
@@ -11,12 +13,16 @@ desired_cap = {'browserName': 'chrome',
                'javascriptEnabled': True}
 
 
-class BaseTest(object):
+class BaseTest(unittest.TestCase):
+    TIMEOUT = 10
 
     config.base_url = get_config_by_key("ui", "url")
+    config.timeout = TIMEOUT
 
-    @pytest.fixture(scope="session", autouse=True)
-    def setup(self):
+    @classmethod
+    def setUpClass(cls):
+        # For remote test execution
+
         # chrome_options = webdriver.ChromeOptions()
         # chrome_options.add_argument("--disable-notifications")
         # chrome_options.add_argument("--enable-automation")
@@ -26,10 +32,10 @@ class BaseTest(object):
         #     command_executor='http://127.0.0.0.1:4444/wd/hub',
         #     desired_capabilities=desired_cap)
 
-        # For remote test execution
-
         driver = webdriver.Chrome(ChromeDriverManager().install())
 
         browser.set_driver(driver)
-        yield
+
+    @classmethod
+    def tearDownClass(cls):
         browser.quit()
